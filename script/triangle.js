eef359bddd382fb4919cb3991485e763c0d7db5a
SubhaeSadeek <nurun.nabi@outlook.com>
2024-04-12 10:43:45 +0600

    catured the input text

--- script/triangle.js
+++ script/triangle.js
@@ -1,6 +1,8 @@
-const triangleBase = document.getElementById("triangle-base").value;
+
 
 function triangleCalculation(){
-    console.log(triangleBase);
+    const triangleBase = document.getElementById("triangle-base");
+    const valueBase = triangleBase.value;
+    console.log(valueBase);
 }
 
