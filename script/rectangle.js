6df3db84abda4f2108b9aaf0e7f2281320ff09fa
SubhaeSadeek <nurun.nabi@outlook.com>
2024-04-13 21:21:14 +0600

    catured the input text

---
+++ script/rectangle.js
@@ -0,0 +1,6 @@
+function rectangleCalc(){
+    const widthRecta = parseFloat(document.getElementById("recta-width").value);
+    const lengthRecta = parseFloat(document.getElementById("recta-length").value);
+    const areaRecta = widthRecta * lengthRecta;
+    console.log(areaRecta);
+}
--- script/triangle.js
+++ script/triangle.js
@@ -1,8 +1,13 @@
 
 
 function triangleCalculation(){
-    const triangleBase = document.getElementById("triangle-base");
-    const valueBase = triangleBase.value;
-    console.log(valueBase);
+    const triangleBase = parseFloat(document.getElementById("triangle-base").value);
+    const triangleHeight = parseInt(document.getElementById("triangle-height").value);
+    const areaTriangle = 0.5 * triangleBase * triangleHeight;
+    
+    const addElement = document.createElement("p");
+    addElement.innerText = "Area of Triangle is = " + areaTriangle;
+    const showTriangleResult = document.getElementById("show-result").append(addElement);
+    console.log(showTriangleResult);
 }
 
